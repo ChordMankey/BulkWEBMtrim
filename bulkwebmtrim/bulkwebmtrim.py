68a3cd3dfae9cedbca4f98289258ded4c51152e8
ChordMankey <ChordMankey@protonmail.com>
2018-06-27 09:27:35 +0530

    Updated to v0.3 From Old Project
    - Can now handle "end" trim.
    - Now handles rotation using 4th column on csv file.
    - Arguments for ffmpeg now stored in two variables for input
    and output.

--- bulkwebmtrim/bulkwebmtrim.py
+++ bulkwebmtrim/bulkwebmtrim.py
@@ -17,70 +17,34 @@ with open(sys.argv[1]) as csvfile:
 
     for row in readCSV:
         output_filename = "{:02} ".format(index) + filename + ' ' + row[2] + '.webm'
+        output_arg = '-c:v libvpx-vp9 -pix_fmt yuv420p -threads 8 -slices 4 -ac 2 -c:a libopus -qmin 28 -crf 30 -qmax 32 -qcomp 1 -b:v 0 -b:a 128000 -vbr on -f webm'
 
         start_h = int(row[0][0:2])
         start_m = int(row[0][2:4])
         start_s = int(row[0][4:6])
-        start = str( start_s + (start_m * 60) + (start_h * 60 * 60) )
+        start = start_s + (start_m * 60) + (start_h * 60 * 60)
 
         if row[1] != 'end':
             stop_h = int(row[1][0:2])
             stop_m = int(row[1][2:4])
             stop_s = int(row[1][4:6])
-            stop = str( stop_s + (stop_m * 60) + (stop_h * 60 * 60) )
-
-            ff = FFmpeg(
-                # global_options='-v quiet -stats',
-
-                inputs={sys.argv[1].split('.')[0] + '.mp4': ['-ss', start,
-                                                             '-to', stop
-                                                             ]},
-
-                outputs={'W:\\WEBM\\' + filename + '\\' + output_filename: ['-c:v', 'libvpx-vp9',
-                                                                            '-pix_fmt', 'yuv420p',
-                                                                            '-threads', '8',
-                                                                            '-slices', '4',
-                                                                            '-ac', '2',
-                                                                            '-c:a', 'libopus',
-                                                                            # '-c:a', 'libvorbis',
-                                                                            '-qmin', '28',
-                                                                            '-crf', '30',
-                                                                            '-qmax', '32',
-                                                                            '-qcomp', '1',
-                                                                            '-b:v', '0',
-                                                                            '-b:a', '128000',
-                                                                            '-vbr', 'on',
-                                                                            # '-qscale:a', '3',
-                                                                            '-f', 'webm'
-                                                                            ]}
-            )
+            stop = stop_s + (stop_m * 60) + (stop_h * 60 * 60)
+            input_arg = '-ss {} -to {}'.format(start, stop)
 
         else:
-            ff = FFmpeg(
-                # global_options='-v quiet -stats',
+            input_arg = '-ss {}'.format(start)
 
-                inputs={sys.argv[1].split('.')[0] + '.mp4': ['-ss', start
-                                                             ]},
 
-                outputs={'W:\\WEBM\\' + filename + '\\' + output_filename: ['-c:v', 'libvpx-vp9',
-                                                                            '-pix_fmt', 'yuv420p',
-                                                                            '-threads', '8',
-                                                                            '-slices', '4',
-                                                                            '-ac', '2',
-                                                                            '-c:a', 'libopus',
-                                                                            # '-c:a', 'libvorbis',
-                                                                            '-qmin', '28',
-                                                                            '-crf', '30',
-                                                                            '-qmax', '32',
-                                                                            '-qcomp', '1',
-                                                                            '-b:v', '0',
-                                                                            '-b:a', '128000',
-                                                                            '-vbr', 'on',
-                                                                            # '-qscale:a', '3',
-                                                                            '-f', 'webm'
-                                                                            ]}
-            )
+        if row[3] == '+':
+            output_arg += ' -vf transpose=clock'
+        elif row[3] == '-':
+            output_arg += ' -vf transpose=cclock'
 
+
+        ff = FFmpeg(
+            inputs={sys.argv[1].split('.')[0] + '.mp4': input_arg},
+            outputs={'W:\\WEBM\\' + filename + '\\' + output_filename: output_arg}
+        )
 
 
         ff.run()
