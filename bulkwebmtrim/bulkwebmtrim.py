a8f6629177681313b40ad8d5f93fee8e97e8afb2
ChordMankey <ChordMankey@protonmail.com>
2018-11-26 10:00:06 +0530

    FAILED Attempt at Including Multiple Trims in Single Video Feature
    - row_parse function starts with a new if condition to check whether
    multiple trims are expected in .csv file.
    - ffmpeg command constructed using information from link in comment.
    - .gitignore updated to exclude .vscode and .idea folders.

--- bulkwebmtrim/bulkwebmtrim.py
+++ bulkwebmtrim/bulkwebmtrim.py
@@ -27,41 +27,106 @@ def convert_video(input_file, input_arg, output_file, output_arg):
 
 def row_parse(row, index, input_file):
     filename = input_file.stem
-    output_filename = "{:02} ".format(index) + filename + ' ' + row[2] + '.webm'
-    output_file = Path(config['OutputDir'] + filename + '\\' + output_filename)
+
     output_arg = '-c:v libvpx-vp9 -pix_fmt yuv420p -threads 8 -slices 4 -ac 2 -c:a libopus -qmin 28 -crf 30 -qmax 32 -qcomp 1 -b:v 0 -b:a 128000 -vbr on -f webm -loglevel error'
 
     global start
-    start = [None] * 10
-    start[0] = int(row[0][0:2])
-    start[1] = int(row[0][2:4])
-    start[2] = int(row[0][4:6])
-    start_time = start[2] + (start[1] * 60) + (start[0] * 60 * 60)
-
     global stop
-    stop = [None] * 10
-    if row[1] != 'end':
-        stop[0] = int(row[1][0:2])
-        stop[1] = int(row[1][2:4])
-        stop[2] = int(row[1][4:6])
-        stop_time = stop[2] + (stop[1] * 60) + (stop[0] * 60 * 60)
-        input_arg = '-ss {} -to {}'.format(start_time, stop_time)
+
+    if '|' in row[0]:
+        # https://superuser.com/questions/681885/how-can-i-remove-multiple-segments-from-a-video-using-ffmpeg
+        input_arg = ''
+        output_arg += ' -filter_complex "'
+        id_char = 'a'
+        multi_index = 0
+
+        for times in row:
+            if '|' in times:
+                start_str = times.split('|')[0]
+                stop_str = times.split('|')[1]
+
+                start = [None] * 10
+                start[0] = int(start_str[0:2])
+                start[1] = int(start_str[2:4])
+                start[2] = int(start_str[4:6])
+                start_time = start[2] + (start[1] * 60) + (start[0] * 60 * 60)
+
+                stop = [None] * 10
+                if stop_str != 'end':
+                    stop[0] = int(stop_str[0:2])
+                    stop[1] = int(stop_str[2:4])
+                    stop[2] = int(stop_str[4:6])
+                    stop_time = stop[2] + (stop[1] * 60) + (stop[0] * 60 * 60)
+                    input_arg = '-ss {} -to {}'.format(start_time, stop_time)
+
+                else:
+                    stop[0] = 99
+                    stop[1] = 99
+                    stop[2] = 99
+                    input_arg = '-ss {}'.format(start_time)
+
+                output_arg += '[0:v]trim=start={}:end={},setpts=PTS-STARTPTS[{}v];'.format(start_time, stop_time, id_char)
+                output_arg += '[0:a]atrim=start={}:end={},asetpts=PTS-STARTPTS[{}a];'.format(start_time, stop_time, id_char)
+
+                # Checking if id_char is greater than 'a'
+                if ord(id_char) > 97:
+                    id_char = chr(ord(id_char)+1)
+                    output_arg += '[{}v][{}v]concat[{}v];'.format(chr(ord(id_char)-2), chr(ord(id_char)-1), id_char)
+                    output_arg += '[{}a][{}a]concat=v=0:a=1[{}a];'.format(chr(ord(id_char)-2), chr(ord(id_char)-1), id_char)
+
+
+                id_char = chr(ord(id_char)+1)
+                multi_index += 1
+            else:
+                id_char = chr(ord(id_char)-1)
+                output_arg += '" -map [{}v] -map [{}a]'.format(id_char, id_char)
+
+                break
+
+        output_filename = "{:02} ".format(index) + filename + ' ' + row[multi_index] + '.webm'
+        multi_index += 1
+
+        try:
+            if row[multi_index] == '+':
+                output_arg += ' -vf transpose=clock'
+            elif row[multi_index] == '-':
+                output_arg += ' -vf transpose=cclock'
+        except IndexError:
+            pass
 
     else:
-        stop[0] = 99
-        stop[1] = 99
-        stop[2] = 99
-        input_arg = '-ss {}'.format(start_time)
+        output_filename = "{:02} ".format(index) + filename + ' ' + row[2] + '.webm'
 
-    try:
-        if row[3] == '+':
-            output_arg += ' -vf transpose=clock'
-        elif row[3] == '-':
-            output_arg += ' -vf transpose=cclock'
-    except IndexError:
-        pass
+        start = [None] * 10
+        start[0] = int(row[0][0:2])
+        start[1] = int(row[0][2:4])
+        start[2] = int(row[0][4:6])
+        start_time = start[2] + (start[1] * 60) + (start[0] * 60 * 60)
+
+        stop = [None] * 10
+        if row[1] != 'end':
+            stop[0] = int(row[1][0:2])
+            stop[1] = int(row[1][2:4])
+            stop[2] = int(row[1][4:6])
+            stop_time = stop[2] + (stop[1] * 60) + (stop[0] * 60 * 60)
+            input_arg = '-ss {} -to {}'.format(start_time, stop_time)
+
+        else:
+            stop[0] = 99
+            stop[1] = 99
+            stop[2] = 99
+            input_arg = '-ss {}'.format(start_time)
+
+        try:
+            if row[3] == '+':
+                output_arg += ' -vf transpose=clock'
+            elif row[3] == '-':
+                output_arg += ' -vf transpose=cclock'
+        except IndexError:
+            pass
 
 
+    output_file = Path(config['OutputDir'] + filename + '\\' + output_filename)
     try:
         convert_video(input_file, input_arg, output_file, output_arg)
     except Exception as e:
